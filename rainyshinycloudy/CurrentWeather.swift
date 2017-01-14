6a641d69c9ff92ff93c2ddc503c7fc5e79c5b1a6
Yalcin Cakmak <yc@YCs-MacBook-Pro.local>
2017-01-13 19:36:46 -0800

    Finished the main parts

---
+++ rainyshinycloudy/CurrentWeather.swift
@@ -0,0 +1,9 @@
+//
+//  CurrentWeather.swift
+//  rainyshinycloudy
+//
+//  Created by YC on 1/3/17.
+//  Copyright © 2017 Cakmak LLC. All rights reserved.
+//
+
+import Foundation
---
+++ rainyshinycloudy/WeatherCell.swift
@@ -0,0 +1,24 @@
+//
+//  WeatherCell.swift
+//  rainyshinycloudy
+//
+//  Created by YC on 1/3/17.
+//  Copyright © 2017 Cakmak LLC. All rights reserved.
+//
+
+import UIKit
+
+class WeatherCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
