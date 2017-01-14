4523014d2c80e232463cbb780550ec1375b2251a
Yalcin Cakmak <yc@YCs-MacBook-Pro.local>
2017-01-13 19:41:56 -0800

    finished the main parts

--- rainyshinycloudy/Constants.swift
+++ rainyshinycloudy/Constants.swift
@@ -7,3 +7,18 @@
 //
 
 import Foundation
+
+let BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
+let LATITUDE = "lat="
+let LONGITUDE = "&lon="
+let APP_ID = "&appid="
+let API_KEY = "264263492b8b9788bbd945ebbf0a23d9"
+
+typealias DownloadComplete = () -> ()
+
+let CURRENT_WEATHER_URL = "\(BASE_URL)\(LATITUDE)\(Location.sharedInstance.latitude!)\(LONGITUDE)\(Location.sharedInstance.longitude!)\(APP_ID)\(API_KEY)"
+
+//let FORECAST_URL = "http://api.openweathermap.org/data/2.5/forecast/daily?lat=-36&lon=123&cnt=10&mode=json&appid=264263492b8b9788bbd945ebbf0a23d9"
+
+
+let FORECAST_URL = "http://api.openweathermap.org/data/2.5/forecast/daily?lat=\(Location.sharedInstance.latitude!)&lon=\(Location.sharedInstance.longitude!)&cnt=10&mode=json&appid=264263492b8b9788bbd945ebbf0a23d9"
--- rainyshinycloudy/Location.swift
+++ rainyshinycloudy/Location.swift
@@ -6,4 +6,18 @@
 //  Copyright © 2017 Cakmak LLC. All rights reserved.
 //
 
-import Foundation
+import CoreLocation
+
+class Location {
+    
+    static var sharedInstance = Location()
+    
+    private init() {}
+    
+    var latitude: Double!
+    var longitude: Double!
+    
+    
+}
+
+
